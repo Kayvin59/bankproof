65f5262078dd2de0881973bd409a03d2d34c37ba
Kayvin <kayvin4@hotmail.com>
2025-04-21 23:35:38 +0200

    feat: setup next js

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+    return (
+      <div className="flex h-full w-96 max-w-full flex-col p-4 md:w-[1008px]">
+        <h1>Bankproof</h1>
+      </div>
+    );
+}
