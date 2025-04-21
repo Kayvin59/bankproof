e00dbd38bd73f065e8f58aa54c258b84670e7a13
Kayvin <kayvin4@hotmail.com>
2025-04-22 00:47:50 +0200

    feat: add shadcn

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,12 @@
+import { Separator } from "../components/ui/separator";
 
 export default function Page() {
     return (
-      <div className="flex h-full max-w-full justify-center p-4 md:w-[1008px]">
-        <h1>Bankproof</h1>
-      </div>
+      <>
+        <div className="flex h-full max-w-full justify-center p-4 md:w-[1008px]">
+          <h1 className="block">Bankproof</h1>
+        </div>
+        <Separator className="my-4" />
+      </>
     );
 }
